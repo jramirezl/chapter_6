de6913569dea095e5f877bcbc62cd45296baefcd
Juan Ramirez <jpramirezl@gmail.com>
2014-02-25 13:06:17 -0600

    Uniqueness validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140225153838) do
+ActiveRecord::Schema.define(:version => 20140225190521) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
